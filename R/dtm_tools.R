da6568592b305ee028c71fa909ba0a45d5f394dc
Nic <sciurusurbanus@hotmail.it>
2026-04-05 19:49:42 +0200

    Update dtm_tools.R

--- R/dtm_tools.R
+++ R/dtm_tools.R
@@ -1,7 +1,7 @@
 make_road_dtm = function(dtm, xc)
 {
-  row <- raster::rowFromY(dtm, xc)
-  roadz <- dtm[raster::cellFromRow(dtm, row)]
+  row <- terra::rowFromY(dtm, xc)
+  roadz <- dtm[terra::cellFromRowCol(dtm, row=row)]
   m = matrix(roadz, nrow(dtm), ncol(dtm), byrow = TRUE)
   road_dtm = dtm
   road_dtm[] = m
