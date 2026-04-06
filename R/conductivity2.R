f3e44a49dfdb4bce4f73fc4f33c546d8bfdf67ca
Nic <sciurusurbanus@hotmail.it>
2026-04-06 18:04:17 +0200

    Update conductivity2.R

--- R/conductivity2.R
+++ R/conductivity2.R
@@ -18,14 +18,14 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   }
   else if (lidR:::is_raster(dtm))
   {
-    res <- round(raster::res(dtm)[1], 2)
+    res <- round(terra::res(dtm)[1], 2)
     if (res > 1) stop("The DTM must have a resolution of 1 m or less.")
 
-    bb = lidR::st_bbox(las)
-    dtm <- raster::crop(dtm, raster::extent(bb))
+    #bb = lidR::st_bbox(las)
+    dtm <- terra::crop(dtm, terra::ext(las))
 
     if (res < 1)
-      dtm <- raster::aggregate(dtm, fact = 1/res, fun = mean)
+      dtm <- terra::aggregate(dtm, fact = 1/res, fun = "mean")
   }
   else
   {
@@ -55,7 +55,7 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
 
   # Force to use raster
   if (lidR:::raster_pkg(dtm) == "terra")
-    dtm <- raster::raster(dtm)
+    dtm <- terra::rast(dtm)
 
   nlas <- lidR::normalize_height(las, dtm) |> suppressMessages() |> suppressWarnings()
   nlas@data[Classification == LASWATER & Z > 2, Classification := LASBRIGDE]
@@ -63,33 +63,33 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   bridge = sf::st_coordinates(bridge, z = FALSE)
 
   # Terrain metrics using the raster package (slope, roughness)
-  slope <- terra::terrain(dtm, opt = c("slope"), unit = "degrees")
-  if (!is.null(mask)) slope <- raster::mask(slope, mask, inverse = T)
-  if(display) raster::plot(slope, col = gray(1:30/30), main = "slope")
+  slope <- terra::terrain(dtm, v = "slope", unit = "degrees")
+  if (!is.null(mask)) slope <- terra::mask(slope, mask, inverse = T)
+  if(display) terra::plot(slope, col = gray(1:30/30), main = "slope")
 
-  smoothdtm = raster::focal(dtm, matrix(1,5,5), mean)
+  smoothdtm = terra::focal(dtm, matrix(1,5,5), fun="mean")
   roughdtm = dtm - smoothdtm
-  roughness <- terra::terrain(roughdtm, opt = c("roughness"), unit = "degrees")
+  roughness <- terra::terrain(roughdtm, v = "roughness", unit = "degrees")
 
   if(display) {
-    raster::plot(roughdtm, col = gray(1:30/30), main = "Residual roughness")
-    raster::plot(roughness, col = gray(1:30/30), main = "Roughness")
+    terra::plot(roughdtm, col = gray(1:30/30), main = "Residual roughness")
+    terra::plot(roughness, col = gray(1:30/30), main = "Roughness")
   }
 
   # Slope-based conductivity
   s <- param$conductivity$s
   sigma_s <- activation(slope, s, "piecewise-linear", asc = FALSE)
-  sigma_s <- raster::aggregate(sigma_s, fact = 2, fun = mean)
+  sigma_s <- terra::aggregate(sigma_s, fact = 2, fun = "mean")
 
-  if (display) raster::plot(sigma_s, col = viridis::viridis(25), main = "Conductivity slope")
+  if (display) terra::plot(sigma_s, col = viridis::viridis(25), main = "Conductivity slope")
   verbose("   - Slope conductivity map \n")
 
   # Roughness-based conductivity
   r <- param$conductivity$r
   sigma_r <- activation(roughness, r, "piecewise-linear", asc = FALSE)
-  sigma_r <- raster::aggregate(sigma_r, fact = 2, fun = mean)
+  sigma_r <- terra::aggregate(sigma_r, fact = 2, fun = "mean")
 
-  if (display) raster::plot(sigma_r, col = viridis::viridis(25), main = "Conductivity roughness")
+  if (display) terra::plot(sigma_r, col = viridis::viridis(25), main = "Conductivity roughness")
   verbose("   - Roughness conductivity map\n")
 
   # Edge-based conductivity
@@ -97,9 +97,9 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   sobl <- sobel.RasterLayer(slope)
   #plot(sigma_e, col = gray(1:30/30))
   sigma_e <- activation(sobl, e, "piecewise-linear", asc = FALSE)
-  sigma_e <- raster::aggregate(sigma_e, fact = 2, fun = mean)
+  sigma_e <- terra::aggregate(sigma_e, fact = 2, fun = "mean")
 
-  if (display) raster::plot(sigma_e, col = viridis::viridis(25), main = "Conductivity Sobel edges")
+  if (display) terra::plot(sigma_e, col = viridis::viridis(25), main = "Conductivity Sobel edges")
   verbose("   - Sobel conductivity map\n")
 
   # Intensity-based conductivity
@@ -107,13 +107,13 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   sigma_i[] <- 0
   if (use_intensity)
   {
-    template2m <- raster::aggregate(dtm, fact = 2)
+    template2m <- terra::aggregate(dtm, fact = 2)
     template2m[] <- 0
     irange = intensity_range_by_flightline(las, template2m)
     #irange = terra::focal(irange, matrix(1,3,3), mean, na.rm = T)
-    if (!is.null(mask)) irange <- raster::mask(irange, mask, inverse = T)
+    if (!is.null(mask)) irange <- terra::mask(irange, mask, inverse = T)
 
-    if (display) raster::plot(irange, col = heat.colors(20), main = "Intensity range")
+    if (display) terra::plot(irange, col = heat.colors(20), main = "Intensity range")
 
     #th <- stats::quantile(irange[], probs = q, na.rm = TRUE)
     th <- c(0.25,0.35)
@@ -121,7 +121,7 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
     sigma_i <- activation(irange, th, "piecewise-linear", asc = FALSE)
     #sigma_i = activation2(irange)
 
-    if (display) raster::plot(sigma_i, col = viridis::viridis(20), main = "Conductivity intensity")
+    if (display) terra::plot(sigma_i, col = viridis::viridis(20), main = "Conductivity intensity")
     verbose("   - Intensity conductivity map\n")
   }
 
@@ -129,12 +129,12 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   # CHM-based conductivity
   h <- param$conductivity$h
   chm <- lidR::grid_canopy(nlas, dtm, lidR::p2r())
-  if (display) raster::plot(chm, col = height.colors(25),  main = "CHM")
+  if (display) terra::plot(chm, col = height.colors(25),  main = "CHM")
   sigma_h <- dtm
   sigma_h <- activation(chm, h, "piecewise-linear", asc = FALSE)
-  sigma_h <- raster::aggregate(sigma_h, fact = 2, fun = mean)
+  sigma_h <- terra::aggregate(sigma_h, fact = 2, fun = "mean")
 
-  if (display) raster::plot(sigma_h, col = viridis::inferno(25), main = "Conductivity CHM")
+  if (display) terra::plot(sigma_h, col = viridis::inferno(25), main = "Conductivity CHM")
   verbose("   - CHM conductivity map\n")
 
   # Lowpoints-based conductivity
@@ -145,10 +145,10 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   lp <- density_lp_by_flightline(nlas, dtm)
   lp[is.na(lp)] = 0
   sigma_lp <- activation(lp, th, "thresholds", asc = FALSE)
-  sigma_lp <- raster::aggregate(sigma_lp, fact = 2, fun = min)
+  sigma_lp <- terra::aggregate(sigma_lp, fact = 2, fun = "min")
 
-  if (display) raster::plot(lp, col = viridis::inferno(20), main = "Number of low point")
-  if (display) raster::plot(sigma_lp, col = viridis::inferno(2), main = "Bottom layer")
+  if (display) terra::plot(lp, col = viridis::inferno(20), main = "Number of low point")
+  if (display) terra::plot(sigma_lp, col = viridis::inferno(2), main = "Bottom layer")
   verbose("   - Bottom layer conductivity map\n")
 
   # Density-based conductivity
@@ -158,20 +158,20 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   d <- density_gnd_by_flightline(las, sigma_lp, drop_angles = 0)
   d[is.na(d)] = 0
   M   <- matrix(1,3,3)
-  d = terra::focal(d, M, mean, na.rm = T)
+  d = terra::focal(d, M, fun="mean", na.rm = T)
   th <- mean(d[d>0], na.rm = T)
   d[is.na(d)] = 0
 
-  if (!is.null(mask)) d <- raster::mask(d, mask, inverse = T, updatevalue = 0)
-  if (display) raster::plot(d, col = viridis::inferno(15), main = "Density of ground points")
+  if (!is.null(mask)) d <- terra::mask(d, mask, inverse = T, updatevalue = 0)
+  if (display) terra::plot(d, col = viridis::inferno(15), main = "Density of ground points")
 
   sigma_d <- activation(d, c(0.33, 0.66), "piecewise-linear")
 
-  if (display)  raster::plot(sigma_d, col = viridis::inferno(25), main = "Conductivity density")
+  if (display)  terra::plot(sigma_d, col = viridis::inferno(25), main = "Conductivity density")
   verbose("   - Density conductivity map\n")
 
   hard_slope = slope < 25
-  hard_slope <- raster::aggregate(hard_slope, fact = 2, fun = min)
+  hard_slope <- terra::aggregate(hard_slope, fact = 2, fun = "min")
 
 
   # Final conductivity sigma
@@ -181,21 +181,21 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
   sigma <- (sigma_e + alpha$d * sigma_d + alpha$h * sigma_h + alpha$r * sigma_r + alpha$i * sigma_i)
   sigma <- sigma/max_coductivity
 
-  if (display) raster::plot(sigma, col = viridis::inferno(25), main = "Conductivity")
+  if (display) terra::plot(sigma, col = viridis::inferno(25), main = "Conductivity")
 
   sigma <- edge_enhancement(sigma, interation = 50, lambda = 0.05, k = 20, pass = 2)
 
-  if (display) raster::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity")
+  if (display) terra::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity")
 
   sigma[sigma < 0.1] = 0.1
-  #raster::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity")
+  #terra::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity")
   sigma[hard_slope == 0] = 0.1
-  #raster::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity")
+  #terra::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity")
 
   sigma[is.na(sigma)] <- 0.1 # lakes
-  cells = raster::cellFromXY(sigma, bridge)
+  cells = terra::cellFromXY(sigma, bridge)
   sigma[cells] = 0.75
-  if (display) raster::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity with bridge")
+  if (display) terra::plot(sigma, col = viridis::inferno(25), main = "Edge enhanced conductivity with bridge")
 
   if (pkg == "terra") sigma <- terra::rast(sigma)
 
@@ -203,13 +203,13 @@ rasterize_conductivity2.LAS <- function(las, dtm = NULL, water = NULL, param = a
 
   if (!is.null(dots$rawlayer))
   {
-   dtm2 = raster::aggregate(dtm, fact = 2, fun = mean)
-   slope2 = raster::aggregate(slope, fact = 2, fun = mean)
-   roughness2 =  raster::aggregate(roughness, fact = 2, fun = mean)
-   chm2 =  raster::aggregate(chm, fact = 2, fun = mean)
-   lp2 =  raster::aggregate(lp, fact = 2, fun = mean)
-   sobl2 =  raster::aggregate(sobl, fact = 2, fun = mean)
-   u = raster::stack(slope2, roughness2, chm2, sigma_i, lp2, d, sobl2)
+   dtm2 = terra::aggregate(dtm, fact = 2, fun = mean)
+   slope2 = terra::aggregate(slope, fact = 2, fun = mean)
+   roughness2 = terra::aggregate(roughness, fact = 2, fun = mean)
+   chm2 = terra::aggregate(chm, fact = 2, fun = mean)
+   lp2 = terra::aggregate(lp, fact = 2, fun = mean)
+   sobl2 = terra::aggregate(sobl, fact = 2, fun = mean)
+   u = c(slope2, roughness2, chm2, sigma_i, lp2, d, sobl2)
    names(u) = c("slope", 'rought', "chm", "intensity", "low", "density", "sobel")
    return(u)
   }
@@ -248,7 +248,7 @@ rasterize_conductivity2.LAScatalog = function(las, dtm = NULL, water = NULL, par
 
 anisotropic_diffusion_filter = function(x, interation = 50, lambda = 0.2, k = 10)
 {
-  M = raster::as.matrix(x)
+  M = terra::as.matrix(x)
   M2 = anisotropic_diffusion(M*255, interation, lambda, k)
   M2 = M2/255
   y = x
@@ -262,7 +262,7 @@ edge_enhancement = function(x, interation = 50, lambda = 0.05, k = 20, pass = 2)
   for (i in 1:pass)
     x = anisotropic_diffusion_filter(x, interation, lambda, k)
 
-  x = raster::stretch(x, minv = 0, maxv = 1, maxq = 0.995)
+  x = terra::stretch(x, minv = 0, maxv = 1, maxq = 0.995)
   x
 }
 
@@ -276,7 +276,7 @@ intensity_range_by_flightline <- function(las, res)
   if (length(ids) == 1)
   {
     ans = rasterize_intensityrange(las, res)
-    return(raster::raster(ans))
+    return(terra::rast(ans))
   }
 
   ans <- vector("list", 0)
@@ -287,13 +287,13 @@ intensity_range_by_flightline <- function(las, res)
     ans[[as.character(i)]] <- rasterize_intensityrange(psi, res)
   }
 
-  ans =terra::rast(ans)
+  ans = terra::rast(ans)
   #plot(ans, col = heat.colors(50))
   ans = terra::stretch(ans, maxv = 1)
   #plot(ans, col = heat.colors(50))
   ans <- terra::app(ans, fun = max, na.rm = TRUE)
   #plot(ans, col = heat.colors(50))
-  return(raster::raster(ans))
+  return(terra::rast(ans))
 }
 
 density_gnd_by_flightline <- function(las, res, drop_angles = 3)
@@ -309,7 +309,7 @@ density_gnd_by_flightline <- function(las, res, drop_angles = 3)
   if (length(ids) == 1)
   {
     ans = rasterize_density(gnd, res)
-    return(raster::raster(ans))
+    return(terra::rast(ans))
   }
 
   ans <- vector("list", 0)
@@ -328,7 +328,7 @@ density_gnd_by_flightline <- function(las, res, drop_angles = 3)
   #plot(ans, col = heat.colors(50))
   ans <- terra::app(ans, fun = max, na.rm = TRUE)
   #plot(ans, col = heat.colors(50))
-  return(raster::raster(ans))
+  return(terra::rast(ans))
 }
 
 density_lp_by_flightline <- function(las, res)
@@ -344,7 +344,7 @@ density_lp_by_flightline <- function(las, res)
   if (length(ids) == 1)
   {
     ans = rasterize_density(gnd, res)
-    return(raster::raster(ans))
+    return(terra::rast(ans))
   }
 
   ans <- vector("list", 0)
@@ -364,7 +364,7 @@ density_lp_by_flightline <- function(las, res)
   #plot(ans, col = heat.colors(50))
   ans <- terra::app(ans, fun = max, na.rm = TRUE)
   #plot(ans, col = heat.colors(50))
-  return(raster::raster(ans))
+  return(terra::rast(ans))
 }
 
 low_points_by_flightline <- function(nlas, res, th = 2)
@@ -380,7 +380,7 @@ low_points_by_flightline <- function(nlas, res, th = 2)
   {
     tmp <- lidR::filter_poi(nlas, Z > z1, Z < z2)
     ans = lidR:::rasterize_fast(tmp, res, 0, "count", pkg = "terra")
-    return(raster::raster(ans > th))
+    return(terra::rast(ans > th))
   }
 
   ans <- vector("list", 0)
@@ -395,7 +395,7 @@ low_points_by_flightline <- function(nlas, res, th = 2)
   #plot(ans, col = heat.colors(50))
   ans <- terra::app(ans, fun = min, na.rm = TRUE)
   #plot(ans, col = heat.colors(50))
-  return(raster::raster(ans))
+  return(terra::rast(ans))
 }
 
 chm_by_flightline <- function(nlas, res)
@@ -408,7 +408,7 @@ chm_by_flightline <- function(nlas, res)
   if (length(ids) == 1)
   {
     ans = rasterize_canopy(nlas, res, p2r())
-    return(raster::raster(ans))
+    return(terra::rast(ans))
   }
 
   ans <- vector("list", 0)
@@ -428,7 +428,7 @@ chm_by_flightline <- function(nlas, res)
   ans =  ans - min(ans[], na.rm = TRUE)
   #plot(ans, col = height.colors(50))
 
-  return(raster::raster(ans))
+  return(terra::rast(ans))
 }
 
 
